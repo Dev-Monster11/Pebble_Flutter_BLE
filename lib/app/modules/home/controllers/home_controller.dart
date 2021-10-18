c1c6e7ca24d205aa515f2b269a90fe591f2d9f80
Dev-Monster11 <softdrink1991@gmail.com>
2021-10-19 00:33:14 +0300

    asdf

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,121 +1,100 @@
 import 'package:get/get.dart';
-// import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 import 'dart:async';
 import 'dart:convert';
 import 'dart:typed_data';
-import 'package:flutter_blue/flutter_blue.dart';
-import 'package:dart:math';
+// import 'package:flutter_blue/flutter_blue.dart';
 
 class HomeController extends GetxController {
-  // final Rx<BluetoothState> _bluetoothState = BluetoothState.UNKNOWN.obs;
-  // final Rx<String> _name = ''.obs;
-  // final Rx<String> _address = ''.obs;
-  // final RxBool discovering = true.obs;
-  // StreamSubscription<BluetoothDiscoveryResult>? _streamSubscription;
-  // List<BluetoothDiscoveryResult> results = <BluetoothDiscoveryResult>[].obs;
-  // static BluetoothConnection? _connection1;
-  // static BluetoothConnection? _connection2;
+  final Rx<BluetoothState> _bluetoothState = BluetoothState.UNKNOWN.obs;
+  final Rx<String> _name = ''.obs;
+  final Rx<String> _address = ''.obs;
+  final RxBool discovering = true.obs;
+  StreamSubscription<BluetoothDiscoveryResult>? _streamSubscription;
+  List<BluetoothDiscoveryResult> results = <BluetoothDiscoveryResult>[].obs;
+  static BluetoothConnection? _connection1;
+  static BluetoothConnection? _connection2;
   static BluetoothDevice? _pebble1;
   static BluetoothDevice? _pebble2;
   int index = 0;
-  // static BluetoothConnection? get connection1 => HomeController._connection1;
-  // static BluetoothConnection? get connection2 => HomeController._connection2;
+  static BluetoothConnection? get connection1 => HomeController._connection1;
+  static BluetoothConnection? get connection2 => HomeController._connection2;
 
-  FlutterBlue flutterBlue = FlutterBlue.instance;
+  // FlutterBlue flutterBlue = FlutterBlue.instance;
+
   @override
   void onInit() async {
     super.onInit();
 
-    flutterBlue.startScan(timeout: Duration(seconds: 4));
-    var subscription = flutterBlue.scanResults.listen((results) {
-      for (ScanResult r in results) {
-        if (r.device.name.contains('SR01')) {
-          index = 1;
-          _pebble1 = r.device;
-          print('Pebble1 address:\t${_pebble1!.name}');
-          flutterBlue.stopScan();
-          break;
-        }
-      }
-    });
+    // flutterBlue.startScan(timeout: Duration(seconds: 4));
 
-    await _pebble1!.connect();
-    List<BluetoothService> services = await _pebble1!.discoverServices();
+    // var subscription = flutterBlue.scanResults.listen((results) {
+    //   for (ScanResult r in results) {
+    //     if (r.device.name.contains('SR01')) {
+    //       index = 1;
+    //       _pebble1 = r.device;
+    //       print('Pebble1 address:\t${_pebble1!.name}');
+    //       flutterBlue.stopScan();
+    //       break;
+    //     }
+    //   }
+    // });
+    // print('subscription----$subscription');
+    // await _pebble1!.connect();
+    // List<BluetoothService> services = await _pebble1!.discoverServices();
 
-    for (BluetoothService s in services) {
-      print('---discovered service is ${s.uuid.toString()}');
-      for (BluetoothCharacteristic c in s.characteristics) {
-        print('---characteristics is ${c.descriptors}');
-        c.write(utf8.encode('COM 090002550003000\r\n'));
-        c.write(utf8.encode('DEL 05000\r\n'));
-        c.write(utf8.encode('WRD pebble1_\r\n'));
-      }
+    // for (BluetoothService s in services) {
+    //   print('---discovered service is ${s.uuid.toString()}');
+    //   for (BluetoothCharacteristic c in s.characteristics) {
+    //     print('---characteristics is ${c.descriptors}');
+    //     c.write(utf8.encode('COM 090002550003000\r\n'));
+    //     c.write(utf8.encode('DEL 05000\r\n'));
+    //     c.write(utf8.encode('WRD pebble1_\r\n'));
+    //   }
+    // }
+    // discovering.value = false;
+    await FlutterBluetoothSerial.instance.requestEnable();
+    final int timeout =
+        (await FlutterBluetoothSerial.instance.requestDiscoverable(60))!;
+    if (timeout < 0) {
+      print('Discoverable mode denied');
+    } else {
+      print('Discoverable mode acquired for $timeout seconds');
     }
 
-    // FlutterBluetoothSerial.instance.requestEnable();
-    // final int timeout =
-    //     (await FlutterBluetoothSerial.instance.requestDiscoverable(60))!;
-    // if (timeout < 0) {
-    //   print('Discoverable mode denied');
-    // } else {
-    //   print('Discoverable mode acquired for $timeout seconds');
-    // }
-    // FlutterBluetoothSerial.instance.state
-    //     .then((value) => {_bluetoothState.value = value});
+    _streamSubscription =
+        FlutterBluetoothSerial.instance.startDiscovery().listen((event) {
+      if (event.device.name!.contains('SR01')) {
+        _pebble1 = event.device;
 
-    // Future.doWhile(() async {
-    //   // Wait if adapter not enabled
-    //   if ((await FlutterBluetoothSerial.instance.isEnabled) ?? false) {
-    //     return false;
-    //   }
-    //   await Future.delayed(Duration(milliseconds: 0xDD));
-    //   return true;
-    // }).then((_) {
-    //   // Update the address field
-    //   FlutterBluetoothSerial.instance.address.then((address) {
-    //     _address.value = address!;
-    //   });
-    // });
-
-    // FlutterBluetoothSerial.instance.name.then((name) {
-    //   _name.value = name!;
-    // });
-
-    // // Listen for futher state changes
-    // FlutterBluetoothSerial.instance
-    //     .onStateChanged()
-    //     .listen((BluetoothState state) {
-    //   _bluetoothState.value = state;
-    // });
-
-    // _streamSubscription =
-    //     FlutterBluetoothSerial.instance.startDiscovery().listen((event) {
-    //   if (event.device.name!.contains('SR01')) {
-    //     _pebble1 = event.device;
-    //     Get.snackbar('Pebble1', _pebble1!.address);
-    //     index = 1;
-    //   }
-    //   if (event.device.name!.contains('SR02')) {
-    //     Get.snackbar('Pebble2', _pebble2!.address);
-    //     _pebble2 = event.device;
-    //     index = index | 2;
-    //   }
-    //   if (index == 3) {
-    //     FlutterBluetoothSerial.instance.cancelDiscovery();
-    //     Get.snackbar("Hi", 'All pebbles are found');
-    //     return;
-    //   }
-    // });
-
-    // _streamSubscription!.onDone(() {
-    //   discovering.value = false;
-    // });
-    // FlutterBluetoothSerial.instance.setPairingRequestHandler((request) => null)
-    // BluetoothConnection.toAddress(_pebble1!.address)
-    //     .then((value) => {_connection1 = value});
+        print('pebble1---${_pebble1!.address}');
+        FlutterBluetoothSerial.instance.cancelDiscovery();
+        index = 1;
+      }
+      // if (event.device.name!.contains('SR02')) {
+      //   Get.snackbar('Pebble2', _pebble2!.address);
+      //   _pebble2 = event.device;
+      //   index = index | 2;
+      // }
+      // if (index == 3) {
+      //   FlutterBluetoothSerial.instance.cancelDiscovery();
+      //   Get.snackbar("Hi", 'All pebbles are found');
+      //   return;
+      // }
+    });
+    print('-----');
+    _streamSubscription!.onDone(() {
+      print('discovering done');
+      discovering.value = false;
+    });
+    // FlutterBluetoothSerial.instance.setPairingRequestHandler((request) => )
+    BluetoothConnection.toAddress(_pebble1!.address).then((value) {
+      _connection1 = value;
+      print('----connection1  ${_connection1!.isConnected}');
+    });
     // BluetoothConnection.toAddress(_pebble2!.address)
     //     .then((value) => {_connection2 = value});
-    // connection1!.input!.listen(_onDataReceived1);
+    connection1!.input!.listen(_onDataReceived1);
     // connection2!.input!.listen(_onDataReceived2);
   }
 
@@ -128,6 +107,21 @@ class HomeController extends GetxController {
   }
 
   void sendData() async {
+    print('---senddata');
+    Get.snackbar('Pebble1', 'Pebble1 Started');
+    connection1!.output
+        .add(Uint8List.fromList(utf8.encode("COM 090002550003000\r\n")));
+    await connection1!.output.allSent;
+
+    Get.snackbar('Pebble1', 'COM command sent');
+    connection1!.output.add(Uint8List.fromList(utf8.encode("DEL 05000\r\n")));
+    await connection1!.output.allSent;
+    Get.snackbar('Pebble1', 'DEL command sent');
+    connection1!.output
+        .add(Uint8List.fromList(utf8.encode("WRD pebble1_\r\n")));
+    await connection1!.output.allSent;
+    Get.snackbar('Pebble1', 'WRD command sent');
+
     // if (index & 1 == 1) {
     //   Get.snackbar('Pebble1', 'Pebble1 Started');
     //   connection1!.output
