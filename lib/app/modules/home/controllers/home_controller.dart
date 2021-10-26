290e295ec4bd6ffad8c85e327628d0e94abc17e5
apollo1130 <yanetolstiy@bk.ru>
2021-10-26 04:09:24 +0300

    asdf

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -7,7 +7,7 @@ class HomeController extends GetxController {
   FlutterBlue flutterBlue = FlutterBlue.instance;
   BluetoothDevice? p1;
   BluetoothDevice? p2;
-  Guid guid1 = Guid('00001800-0000-1000-8000-00805F9B34FB');
+  // Guid guid1 = Guid('00001800-0000-1000-8000-00805F9B34FB');
   Guid guid2 = Guid('6E400001-B5A3-F393-E0A9-E50E24DCCA9E');
   var p1Found = false.obs;
   var p2Found = false.obs;
@@ -16,7 +16,7 @@ class HomeController extends GetxController {
     p1Found.value = false;
     p2Found.value = false;
     isScanning.value = 1;
-    flutterBlue.startScan(timeout: Duration(seconds: 2));
+    flutterBlue.startScan(timeout: Duration(seconds: 2), withServices: [guid2]);
     // flutterBlue.startScan(timeout: Duration(seconds: 2));
 
     flutterBlue.scanResults.listen((event) {
@@ -70,18 +70,18 @@ class HomeController extends GetxController {
       var characteristics = service.characteristics;
 
       for (BluetoothCharacteristic c in characteristics) {
-        if (c.uuid == guid1 || c.uuid == guid2) {
-          await c.write(utf8.encode('COM 090002550003000'));
-          List<int> v = await c.read();
-          print('read--------$v');
-          await c.write(utf8.encode('DEL 05000'));
-          v = await c.read();
-          print('del read-------$v');
-          await c.write(utf8.encode('WRD pebble2_'));
-          v = await c.read();
-          print('wrd read--------$v');
-          return 10;
-        }
+        // if (c.uuid == 0x03) {
+        await c.write(utf8.encode('COM 090002550003000'));
+        List<int> v = await c.read();
+        print('read--------$v');
+        await c.write(utf8.encode('DEL 05000'));
+        v = await c.read();
+        print('del read-------$v');
+        await c.write(utf8.encode('WRD pebble2_'));
+        v = await c.read();
+        print('wrd read--------$v');
+        return 10;
+        // }
       }
     }
     return 0;
